fdd26de0d6e06d6d8abdf7d01188a3d67130423e
Chris <chrisrkemp@hotmail.co.uk>
2019-03-25 15:03:48 +0000

    Commit of ItemBase

---
+++ Source/UnrealThirdPerson/Actors/Items/ItemBase.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ItemBase.generated.h"
+
+UCLASS()
+class UNREALTHIRDPERSON_API AItemBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AItemBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+protected:
+	//Name to identify the item type
+	UPROPERTY(EditAnywhere)
+	FString Name;
+	//Provide further information when in inventory
+	UPROPERTY(EditAnywhere)
+	FString Description;
+	//
+	UPROPERTY(EditAnywhere)
+	class UMeshComponent* MeshComponent;
+	/*
+	UPROPERTY(EditAnywhere)
+	UImage* Icon;
+	*/
+};
